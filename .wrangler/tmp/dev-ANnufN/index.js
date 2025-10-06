55dc4e83904a003083650af41a737de5f64ef6fe
Gilson Fonseca <gildevson@gmail.com>
2025-10-06 00:08:34 -0300

    Adicionando mais dados

--- .wrangler/tmp/dev-ANnufN/index.js
+++ .wrangler/tmp/dev-ANnufN/index.js
@@ -1393,13 +1393,13 @@ var require_dayjs_min = __commonJS({
   }
 });
 
-// .wrangler/tmp/bundle-pQo35v/middleware-loader.entry.ts
+// .wrangler/tmp/bundle-TJZyox/middleware-loader.entry.ts
 init_modules_watch_stub();
 init_virtual_unenv_global_polyfill_cloudflare_unenv_preset_node_process();
 init_virtual_unenv_global_polyfill_cloudflare_unenv_preset_node_console();
 init_performance2();
 
-// .wrangler/tmp/bundle-pQo35v/middleware-insertion-facade.js
+// .wrangler/tmp/bundle-TJZyox/middleware-insertion-facade.js
 init_modules_watch_stub();
 init_virtual_unenv_global_polyfill_cloudflare_unenv_preset_node_process();
 init_virtual_unenv_global_polyfill_cloudflare_unenv_preset_node_console();
@@ -33739,7 +33739,7 @@ var jsonError = /* @__PURE__ */ __name(async (request, env2, _ctx, middlewareCtx
 }, "jsonError");
 var middleware_miniflare3_json_error_default = jsonError;
 
-// .wrangler/tmp/bundle-pQo35v/middleware-insertion-facade.js
+// .wrangler/tmp/bundle-TJZyox/middleware-insertion-facade.js
 var __INTERNAL_WRANGLER_MIDDLEWARE__ = [
   middleware_ensure_req_body_drained_default,
   middleware_miniflare3_json_error_default
@@ -33775,7 +33775,7 @@ function __facade_invoke__(request, env2, ctx, dispatch, finalMiddleware) {
 }
 __name(__facade_invoke__, "__facade_invoke__");
 
-// .wrangler/tmp/bundle-pQo35v/middleware-loader.entry.ts
+// .wrangler/tmp/bundle-TJZyox/middleware-loader.entry.ts
 var __Facade_ScheduledController__ = class ___Facade_ScheduledController__ {
   constructor(scheduledTime, cron, noRetry) {
     this.scheduledTime = scheduledTime;
